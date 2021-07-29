f621608351314a0ebc76e76632d52783b409583c
sarinpost <sarin.chococrm@gmail.com>
2021-07-29 22:37:33 +0700

    add button

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <template>
-  <router-view />
+  <div>
+    <button @click="$router.push({ name: 'q1' })">question 1</button>
+    <button @click="$router.push({ name: 'q2' })">question 2</button>
+    <router-view />
+  </div>
 </template>
 
 <script>
